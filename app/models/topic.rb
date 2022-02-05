f1001487c8a8e0430b7d93ce86df52df632f1631
Kurosawa <tadatadatanni.knss@me.com>
2022-02-05 09:46:03 +0000

    fourth commit

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -10,7 +10,5 @@ class Topic < ApplicationRecord
   
   mount_uploader :image, ImageUploader
   
-  has_many :favorites
-  has_many :favorite_users, through: :favorites, source: 'user'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'menus/term'
   get 'menus/question'
   get 'menus/registation'
+  get 'topics/index'
+  get 'topics/new'
   get 'search' => 'homes#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
@@ -23,6 +25,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :posts
+  resources :topics
   
   get 'favorites/index'
   post '/favorites', to: 'favorites#create'
